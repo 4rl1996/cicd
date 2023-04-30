7e26ed9b32b694f3b23523f1adc96d3f3a33b16e
Roman <you@example.com>
2023-04-30 17:37:54 +0300

    ssh

--- start-script.sh
+++ start-script.sh
@@ -17,7 +17,5 @@ else
     printf "Start $APP with 'nohup' command.\n"
     nohup mvn clean package -DskipTests spring-boot:run > log &
 
-    printf "Please, wait for 'tail -f log' output.\n"
-    sleep 5
-    tail -f log #ctrl+c to exit tail -f output
+    printf "Please, 'tail -f log' for log output.\n"
 fi
